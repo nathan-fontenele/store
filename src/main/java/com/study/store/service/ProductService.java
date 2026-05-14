b67717928aa824454c29ebdd0449213f18246767
Nathan Fontenele <nathan.fontenele@gondimadv.com.br>
2026-05-14 19:57:22 -0300

    refactor: add @Transactional and replace RuntimeException with ProductNotFoundException

--- src/main/java/com/study/store/service/ProductService.java
+++ src/main/java/com/study/store/service/ProductService.java
@@ -2,20 +2,22 @@ package com.study.store.service;
 
 import com.study.store.dto.ProductRequest;
 import com.study.store.dto.ProductResponse;
+import com.study.store.exceptions.ProductNotFoundException;
 import com.study.store.model.Product;
-import com.study.store.repository.ProductReposity;
+import com.study.store.repository.ProductRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class ProductService {
-    private final ProductReposity productRepository;
+    private final ProductRepository productRepository;
 
-    public ProductService(ProductReposity productRepository) {
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<ProductResponse> findAll(){
         return productRepository.findAll()
                 .stream()
@@ -23,11 +25,13 @@ public class ProductService {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     public ProductResponse findById(Long id){
         Product product = findProductById(id);
         return toResponse(product);
     }
 
+    @Transactional
     public ProductResponse create(ProductRequest request){
         Product product = new Product();
 
@@ -41,6 +45,7 @@ public class ProductService {
         return toResponse(savedProduct);
     }
 
+    @Transactional
     public ProductResponse update(Long id, ProductRequest request) {
         Product product = findProductById(id);
 
@@ -54,6 +59,7 @@ public class ProductService {
         return toResponse(updatedProduct);
     }
 
+    @Transactional
     public void delete(Long id) {
         Product product = findProductById(id);
         productRepository.delete(product);
@@ -61,7 +67,7 @@ public class ProductService {
 
     private Product findProductById(Long id) {
         return productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Produto não encontrado com ID: " + id));
+                .orElseThrow(() -> new ProductNotFoundException(id));
     }
 
     private ProductResponse toResponse(Product product)
